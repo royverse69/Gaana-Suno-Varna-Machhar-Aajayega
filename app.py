2c952469738502abdc36c6cd2b9f8a253765ceae
royverse <154247118+royverse69@users.noreply.github.com>
2026-06-17 19:12:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,32 @@
 
+from flask import Flask, request
+from flask_cors import CORS
+import requests
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def home():
+    return {
+        "status": "Royverse Music API Running"
+    }
+
+@app.route("/search")
+def search():
+    q = request.args.get("q")
+
+    r = requests.get(
+        f"https://music-api.albatross0071.workers.dev/api/search?query={q}"
+    )
+
+    return r.text
+
+@app.route("/song/<song_id>")
+def song(song_id):
+
+    r = requests.get(
+        f"https://music-api.albatross0071.workers.dev/api/songs/{song_id}"
+    )
+
+    return r.text
